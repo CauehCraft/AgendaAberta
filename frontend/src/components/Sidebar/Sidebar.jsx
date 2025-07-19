dc6c38282e229dd528f7b1349300795a534a18f8
dayvisonmsilva <dayvisoneryc@gmail.com>
2025-07-18 21:04:55 -0300

    format(SideBar): apenas formatação do arquivo .jsx

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,8 +1,17 @@
-import { useState } from 'react';
-import { NavLink, useNavigate } from 'react-router-dom';
-import { useAuth } from '../../hooks/useAuth';
-import { FaUserCircle, FaCalendarPlus, FaClock, FaEdit, FaCalendarAlt, FaSignOutAlt, FaBars, FaTimes } from 'react-icons/fa';
-import './Sidebar.css';
+import { useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import { useAuth } from "../../hooks/useAuth";
+import {
+  FaUserCircle,
+  FaCalendarPlus,
+  FaClock,
+  FaEdit,
+  FaCalendarAlt,
+  FaSignOutAlt,
+  FaBars,
+  FaTimes,
+} from "react-icons/fa";
+import "./Sidebar.css";
 
 const Sidebar = () => {
   const { user, logout } = useAuth();
@@ -11,7 +20,7 @@ const Sidebar = () => {
 
   const handleLogout = () => {
     logout();
-    navigate('/login');
+    navigate("/login");
   };
 
   const toggleMenu = () => {
@@ -24,28 +33,45 @@ const Sidebar = () => {
         <FaBars />
       </button>
 
-      <aside className={`sidebar ${isMenuOpen ? 'open' : ''}`}>
+      <aside className={`sidebar ${isMenuOpen ? "open" : ""}`}>
         <button className="sidebar-close-btn" onClick={toggleMenu}>
-            <FaTimes />
+          <FaTimes />
         </button>
 
         <div className="sidebar-profile">
           <FaUserCircle className="sidebar-icon profile-icon" />
           <div>
             {user && <h3>{user.nome_completo || user.username}</h3>}
-            {user && <p>{user.tipo.charAt(0).toUpperCase() + user.tipo.slice(1)}</p>}
+            {user && (
+              <p>{user.tipo.charAt(0).toUpperCase() + user.tipo.slice(1)}</p>
+            )}
           </div>
         </div>
 
         <nav className="sidebar-nav">
           <ul>
             {/* <li><NavLink to="/dashboard/criar-agenda" onClick={toggleMenu}><FaCalendarPlus className="sidebar-icon" /><span>Criar Agenda</span></NavLink></li>*/}
-            <li><NavLink to="/dashboard/adicionar-horario" onClick={toggleMenu}><FaClock className="sidebar-icon" /><span>Adicionar Horário</span></NavLink></li>
-            <li><NavLink to="/dashboard/editar-horario" onClick={toggleMenu}><FaEdit className="sidebar-icon" /><span>Editar/Deletar Horário</span></NavLink></li>
-            <li><NavLink to="/dashboard/visualizar-agenda" onClick={toggleMenu}><FaCalendarAlt className="sidebar-icon" /><span>Visualizar Agenda</span></NavLink></li>
+            <li>
+              <NavLink to="/dashboard/adicionar-horario" onClick={toggleMenu}>
+                <FaClock className="sidebar-icon" />
+                <span>Adicionar Horário</span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/dashboard/editar-horario" onClick={toggleMenu}>
+                <FaEdit className="sidebar-icon" />
+                <span>Editar/Deletar Horário</span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/dashboard/visualizar-agenda" onClick={toggleMenu}>
+                <FaCalendarAlt className="sidebar-icon" />
+                <span>Visualizar Agenda</span>
+              </NavLink>
+            </li>
           </ul>
         </nav>
-        
+
         <div className="sidebar-footer">
           <button className="logout-button" onClick={handleLogout}>
             <FaSignOutAlt className="sidebar-icon" />
